f0e6ae4eaef60e17f1a24970c28b4c2d0b9a9ec3
ehalt <torikussadikk@gmail.com>
2026-06-07 15:22:56 +0600

    home page updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 // import React from 'react'
 
+import { Link } from "react-router-dom";
+
 const Home = () => {
     const posts = [
     {
@@ -37,6 +39,13 @@ const Home = () => {
             <div className="post" key={post.id}>
               <div className="img">
                 <img src={post.img} alt="" />
+              </div>
+              <div className="content">
+                <Link className="link" to={`/post/${post.id}`}>
+                  <h1>{post.title}</h1>
+                </Link>
+                <p>{post.desc}</p>
+                <button>Read more</button>
               </div>
             </div>
           ))
